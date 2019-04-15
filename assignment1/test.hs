cce8da12a04f611a83e4a74e03a8159c8ce5edc3
zhuom2017 <32209316+zhuom2017@users.noreply.github.com>
2019-04-15 16:28:24 -0700

    Add files via upload

--- assignment1/test.hs
+++ assignment1/test.hs
@@ -1,5 +1,5 @@
 -- this is exercise two
-import HW1types 
+import HW1types
 
 g :: Graph
 g = [(1,2),(1,3),(2,3),(2,4),(3,4)]
@@ -7,34 +7,25 @@ g = [(1,2),(1,3),(2,3),(2,4),(3,4)]
 h :: Graph
 h = [(1,2),(1,3),(2,1),(3,2),(4,4)]
 
-firstnode :: Edge -> Node
-firstnode (x,_) = x
-
-secondnode :: Edge -> Node
-secondnode (_,x) = x
-
 ----------------a----------------
 
-nodes :: Graph -> [Node] 
+nodes :: Graph -> [Node]
 nodes []=[]
-nodes (x:xs)= nodes xs ++ [firstnode x] ++ [secondnode x]
-
-gnetlist = norm (nodes g)
-hnetlist = norm (nodes h)
+nodes (x:xs)= norm(nodes xs ++ [fst x] ++ [snd x])
 -----------------b--------------
 
 suc :: Node -> Graph -> [Node]
 suc n [] = []
-suc n (x:xs) = if (firstnode x) == n then [secondnode x] ++ suc n xs
+suc n (x:xs) = if (fst x) == n then [snd x] ++ suc n xs
                else suc n xs
 
-gsuccessor = suc 2 g               
+gsuccessor = suc 2 g
 hsuccessor = suc 4 h
 -----------------c--------------
 
 detach :: Node -> Graph -> Graph
 detach n [] = []
-detach n (x:xs) = if (firstnode x) == n  || (secondnode x) == n then detach n xs
+detach n (x:xs) = if (fst x) == n  || (snd x) == n then detach n xs
                   else [x] ++ detach n xs
 
 gdetachedgraph = detach 3 g
@@ -42,7 +33,6 @@ hdetachedgraph = detach 2 h
 ----------------d---------------
 
 cyc :: Int -> Graph
-cyc 1 = []
-cyc n = cyc(n-1) ++ [((n-1),n)] 
-
-cycgraph = cyc 4 ++ [(4,1)] 
+cyc n | n == 0 = []
+      | n == 1 = [(1,1)]
+      | n > 1 = zip [1..n] ([2..n]++[1])
